05fb6c9cde24bd94099d9845eb3c8e69e0f7f389
Milan Stojkovic <milanstojkovic2707@gmail.com>
2024-03-23 18:41:47 +0100

    refactor: quote entity holds counts

---
+++ QuotesApi/Migrations/20240323155701_QuoteRatingCounters.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuotesApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class QuoteRatingCounters : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NegativeCount",
+                table: "Quotes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PositiveCount",
+                table: "Quotes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NegativeCount",
+                table: "Quotes");
+
+            migrationBuilder.DropColumn(
+                name: "PositiveCount",
+                table: "Quotes");
+        }
+    }
+}
--- QuotesApi/Models/Quote.cs
+++ QuotesApi/Models/Quote.cs
@@ -9,4 +9,6 @@ public class Quote
     public string Author { get; set; } = string.Empty;
     public List<string> Tags { get; set; } = new();
     public List<Rating> RatingList { get; set; } = new();
+    public int PositiveCount { get; set; } = 0;
+    public int NegativeCount { get; set; } = 0;
 }
